cf4091eea3b385a7faff8b3830d041ef68f5a1c4
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2020-11-06 23:12:44 +0800

    ascii gif fail

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,91 +1,188 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import "./MainStyle.scss"
+// const fs = require('fs');
+const extractFrames = require('gif-extract-frames')
+ 
+
+
 const Main = () => {
-    // const [imgSource, setImg] = useState(null)
+    const [frames, setFrames] = useState(null)
 
     const canvasRef = useRef(null)
     const preRef = useRef(null)
-    // const imgRef = useRef(null)
+    const imgRef = useRef(null)
+    const frameRef = useRef(null)
 
     const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
+    const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
+    const rampLength = grayRamp.length;
+    const MAXIMUM_WIDTH = 80;
+    const MAXIMUM_HEIGHT = 80;
 
+    const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
 
-    const handleChange = e => {
-        const file = e.target.files[0];
+    const convertToGrayScales = (context, width, height) => {
+        const imageData = context.getImageData(0, 0, width, height);
+    
         const grayScales = [];
-        
-        const reader = new FileReader();
+    
+        for (let i = 0 ; i < imageData.data.length ; i += 4) {
+            const r = imageData.data[i];
+            const g = imageData.data[i + 1];
+            const b = imageData.data[i + 2];
+    
+            const grayScale = toGrayScale(r, g, b);
+            imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
+    
+            grayScales.push(grayScale);
+        }
+    
+        context.putImageData(imageData, 0, 0);
+    
+        return grayScales;
+    };
+    const getGrayScales = (array) => {
+    
+        const grayScales = [];
+    
+        for (let i = 0 ; i < array.length ; i += 4) {
+            const r = array[i];
+            const g = array[i + 1];
+            const b = array[i + 2];
+    
+            const grayScale = toGrayScale(r, g, b);
+            array[i] = array[i + 1] = array[i + 2] = grayScale;
+    
+            grayScales.push(grayScale);
+        }
+    
+    
+        return grayScales;
+    };
+
+    const getFontRatio = () => {
+        const pre = document.createElement('pre');
+        pre.style.display = 'inline';
+        pre.textContent = ' ';
+    
+        document.body.appendChild(pre);
+        const { width, height } = pre.getBoundingClientRect();
+        document.body.removeChild(pre);
+    
+        return height / width;
+    };
+    
+
+    const clampDimensions = (width, height) => {
+        const rectifiedWidth = Math.floor(getFontRatio() * width);
+        if (height > MAXIMUM_HEIGHT) {
+            const reducedWidth = Math.floor(rectifiedWidth * MAXIMUM_HEIGHT / height);
+            return [reducedWidth, MAXIMUM_HEIGHT];
+        }
+
+        if (width > MAXIMUM_WIDTH) {
+            const reducedHeight = Math.floor(height * MAXIMUM_WIDTH / rectifiedWidth);
+            return [MAXIMUM_WIDTH, reducedHeight];
+        }
+
+        return [rectifiedWidth, height];
+    };
+
+    const drawAscii = (grayScales, width) => {
+        const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
+            let nextChars = getCharacterForGrayScale(grayScale);
+            if ((index + 1) % width === 0) {
+                nextChars += '\n';
+            }
+    
+            return asciiImage + nextChars;
+        }, '');
+    
+        preRef.current.textContent = ascii;
+    };
+
+
+
+
+
+    const handleChange = async e => {
+       
+            const file = e.target.files[0];
+            
+
+            const canvas = canvasRef.current
+            const context = canvas.getContext('2d')
+            const reader = new FileReader();
             reader.onload = (event) => {
                 const image = new Image();
                 image.onload = () => {
-                    const canvas = canvasRef.current
-                    const context = canvas.getContext('2d')
-                    canvas.height = image.height
-                    canvas.width = image.width
-                    context.drawImage(image, 0, 0, image.width, image.height);
-                    const imageData = context.getImageData(0, 0, image.width, image.height);
-
-                    
-
-                    for (let i = 0 ; i < imageData.data.length ; i += 4) {
-                        const r = imageData.data[i];
-                        const g = imageData.data[i + 1];
-                        const b = imageData.data[i + 2];
-
-                        const grayScale = toGrayScale(r, g, b);
-                        imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
-
-                        grayScales.push(grayScale);
-                    }
-
-                    console.log(imageData)
-                    console.log(grayScales)
-                    
-
-                    context.putImageData(imageData, 0, 0);
-                    // return grayScales;
+                    const [width, height] = clampDimensions(image.width, image.height);
+                    imgRef.current.width = image.width
+                    imgRef.current.height = image.height
+                    canvas.width = width;
+                    canvas.height = height;
+        
+                    context.drawImage(image, 0, 0, width, height);
+                    const grayScales = convertToGrayScales(context, width, height);
+                    // console.log(grayScales.length)
+                    drawAscii(grayScales, width);
                 }
-
+        
                 image.src = event.target.result;
 
-                const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
-                const rampLength = grayRamp.length;
-                const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
-                preRef.current.textContent = grayScales.map( (grayScale, index) => index+1 % image.width === 0 ? getCharacterForGrayScale(grayScale) + "123" : getCharacterForGrayScale(grayScale) )
+               
+
+ 
             };
-
+            imgRef.current.src = (URL.createObjectURL(file))
             reader.readAsDataURL(file);
+            // console.log(imgRef.current.src)
 
-           
+            const results = await extractFrames({
+                input: imgRef.current.src,
+                // output: 'frame-%d.png'
+              })
+            const arr = Array.from(results.data)
+            const grayscales = getGrayScales(arr)
+            const dataPerFrame = grayscales.slice().length
+            const frameData = new Array(results.shape[0])
+            // console.log(grayscales.length)
+            // console.log(dataPerFrame)
+            const frameDataArray = frameData.fill().map(_ => grayscales.splice(0, dataPerFrame/results.shape[0]))
+            
+            const frameASCII = frameDataArray.map( (data) => {
+                return data.map((grayScale, index) => {
+                    let nextChars = getCharacterForGrayScale(grayScale);
+                    // console.log(nextChars, index)
+                    // if ((index + 1) % imgRef.current.width === 0) {
+                    //     nextChars += '\n';
+                    // }
+            
+                    return ((index + 1) % imgRef.current.width === 0) ? nextChars + '\n' : nextChars;
+                }).join('')
+                // }, '')
+            // console.log(data)
+
+            } ).join('')
+            console.log(frameASCII)
+            setFrames(frameASCII)
+
+
     }
-    // const handleClick = () => {
-        
-    // }
 
-    // const handleMouse = (event) => {
-    //     const canvas = canvasRef.current
-    //     canvas.height = imgRef.current.height
-    //     canvas.width = imgRef.current.width
-    //     canvas.getContext('2d').drawImage(imgRef.current, 0, 0, imgRef.current.width, imgRef.current.height);
-    //     const pixelData = canvas.getContext('2d').getpixelData(event.nativeEvent.offsetX, event.nativeEvent.offsetY, 1, 1).data
-    //     const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
-    //     const rampLength = grayRamp.length;
-    //     const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
-    //     console.log( getCharacterForGrayScale(toGrayScale(pixelData[0],pixelData[1],pixelData[2])))
-    //     // console.log(String.fromCharCode(pixelData[0] + pixelData[1] + pixelData[2] / 3))
-
-    //     // console.log(event.nativeEvent.offsetX, event.nativeEvent.offsetY)
-    // }
   
 
     return (
         <div className="main-wrapper" >
-            <input type="file"  accept="image/*" name="image" id="file" onChange={ handleChange }  />
+            <input type="file"  accept="image/gif" name="image" id="file" onChange={ handleChange }  />
             <div className="img-wrapper" >
-                {/* <img id="output" src={imgSource} ref={imgRef}  /> */}
+                <img id="output" ref={imgRef}  />
                 <canvas ref={canvasRef} />
                 <pre ref={preRef}></pre>
             </div>
+            <div id='frames' ref={frameRef} ></div>
+            
+            {frames && <pre> {frames} </pre>}
             
             {/* <button onClick={handleClick} > Click </button> */}
         </div>
