a14783084d27711c66303c6f202857094a3d42b3
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2020-11-09 14:44:08 +0800

    ascii gif almost done, not clean

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,30 +1,21 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import "./MainStyle.scss"
 var gifFrames = require('gif-frames');
-var fs = require('fs');
+// var fs = require('fs');
 
 
 const Main = () => {
     const [frames, setFrames] = useState(null)
-
-    const canvasRef = useRef(null)
-    const preRef = useRef(null)
-    const imgRef = useRef(null)
-    const frameRef = useRef(null)
-
+    const [err, setErr] = useState(false)
     const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
     const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
     const rampLength = grayRamp.length;
     const MAXIMUM_WIDTH = 80;
     const MAXIMUM_HEIGHT = 80;
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext('2d')
 
     const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
-    let framecount = 0
-    useEffect(() => {
-        
-        console.log(framecount)
-        framecount+=1
-    }, [frames])
 
     const getGrayScales = (array) => {
     
@@ -78,76 +69,82 @@ const Main = () => {
         let data = null
         data = asciiGIF
         let i = 0
-        console.log(data.length)
+        // console.log(data)
         setInterval(() => {
-            if(i === data.length-1) i = 0
+            if(i === data.length) i = 0
             setFrames(data[i])
+            // console.log(i)
             i++
         }, 100)
-
-        return null
+        // setFrames(asciiGIF) 
+        // setFrames(null)
     }
 
 
-
-
-    const handleChange = e => {
-       
-            const file = e.target.files[0];
-            
-            let asciiGIF = 0
-            const canvas = canvasRef.current
-            const context = canvas.getContext('2d')
-            imgRef.current.src = null
-            imgRef.current.src = URL.createObjectURL(file)
-
-            gifFrames({ url: imgRef.current.src, frames: '0-99', outputType: 'canvas' })
+    const fetchGIF = file => {
+        gifFrames({ url: file, frames: '0-199', outputType: 'canvas' })
                     .then(function (frameData) {
-                    let gifWidth, gifHeight, gifArea
+                    let gifWidth
+                    context.clearRect(0, 0, canvas.width, canvas.height)
                     const gifFrameData = frameData.map( frame => {
                             const [width, height] = clampDimensions(frame.getImage().width, frame.getImage().height);
                             context.drawImage(frame.getImage(), 0, 0, width, height);
                             const gifData = Array.from(context.getImageData(0, 0, width, height).data)
+                            
                             gifWidth = width
-                            gifHeight = height
-                            gifArea = width * height
                         return gifData
                         }  );
-                        asciiGIF = gifFrameData.map( (data) => {
+                        const asciiGIF = gifFrameData.map( (data) => {
                                 // console.log(data)
                                 const gray = getGrayScales(data) 
                                 return gray.map((grayScale, idx) => {
                                     let nextChars = getCharacterForGrayScale(grayScale);
-                                    return ((idx + 1) % gifWidth === 0) ? nextChars + '\n' : nextChars;
+                                    return (!((idx + 1) % gifWidth === 0)) ? nextChars : nextChars + '\n';
                                 }).join('')
                                 // }, '')
 
                                 
                             })
                             animateASCII(asciiGIF)
-                            // console.log(asciiGIF)
-                    }).catch(console.error.bind(console));
-           
+                            setErr(false)
 
-                    return null
+                    }).catch( console.error.bind(console) && setErr(true));
+    }
 
-            
+    const handleChangeURL = e => {
+        if(e.keyCode === 13){
+            fetchGIF(e.target.value)
+        }
+    }
 
+
+    const handleChange = e => {
+            fetchGIF(URL.createObjectURL(e.target.files[0]))
     }
 
   
 
     return (
         <div className="main-wrapper" >
-            <input type="file"  accept="image/gif" name="image" id="file" onChange={ handleChange }  />
-            <div className="img-wrapper" >
-                <img id="output" ref={imgRef}  />
-                <canvas ref={canvasRef} id="gray" />
-                <pre ref={preRef}></pre>
-            </div>
-            <div id='frames' ref={frameRef} ></div>
             
-            {frames && <pre > {frames} </pre> }
+            {/* <div className="img-wrapper" > */}
+                {/* <img id="output" ref={imgRef}  /> */}
+                {/* <canvas ref={canvasRef} id="gray" /> */}
+                {/* <pre ref={preRef}></pre> */}
+            {/* </div> */}
+            {/* <div id='frames' ref={frameRef} ></div> */}
+            
+            {/* {frames && frames.map( frame => <pre > {frame} </pre> )} */}
+
+            <h2> ASCII GIF Converter </h2>
+            <p></p>
+            {frames ? <pre>{frames}</pre> : 
+                <div className="inputs" >
+                    <input type="file"  accept="image/gif" name="image-upload" id="upload" onChange={ handleChange }  />
+                    <input type="url"  name="image-url" id="url" onKeyDown={handleChangeURL}  />
+                </div>
+            }
+            {err && <p>Please upload GIF file</p>}
             
             {/* <button onClick={handleClick} > Click </button> */}
         </div>
